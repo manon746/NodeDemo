41a2a53d2c65eaee0b0469a351244d59571cba54
manon746 <dave200111@hotmail.com>
2022-08-26 11:46:05 -0400

    first commit

---
+++ contact.js
@@ -0,0 +1,10 @@
+class Contact{
+    constructor(name, email, phone) {
+        this.id=0;
+        this.Name=name;
+        this.Email=email;
+        this.Phone=phone;
+    }
+
+}
+module.exports = Contact;
---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("ca marche");
+
+let firstname = "Julie";
+let last = "Lapierre";
+let sex = 1;//woman
+let nom = firstname+ " " + last;
+if (sex==1){
+    console.log(`Hello miss ${nom}`);
+}
+else{
+    console.log(`Hello mrs ${nom}`);
+}
+
+const Contact = require("./contact.js");
+let contact = new Contact("Nicolas","nicolas@gmail.com","(514-333-5656");
+console.log(contact);
+
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    console.log(req);
+
+});
+const Port = process.env.Port || 5000;
+server.listen(Port,()=>console.log(`server running on port ${Port}`));
